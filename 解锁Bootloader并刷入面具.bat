023f2b673cc6599f06f6183ce79db830283b3979
rk3288 <m18749410674@163.com>
2025-03-10 15:22:28 +0800

    Update 解锁Bootloader并刷入面具.bat

--- 解锁Bootloader并刷入面具.bat
+++ 解锁Bootloader并刷入面具.bat
@@ -1,24 +1,24 @@
 @echo off
 mode con cols=100 lines=30
-title ��������ڱ��������ʾ[ѡ��]�������밴�»س���������Ҽ����ѡ�񣬷�����ִͣ�������
+title ←如果窗口标题左侧显示[选择]字样，请按下回车或按下鼠标右键清除选择，否则将暂停执行输出。
 
 if %PROCESSOR_ARCHITECTURE%==x86 (set cpuArch=x86) else set cpuArch=amd64
 
 
 echo.
 echo.
-echo.ע��ò�����Root����豸�������������
+echo.注意该操作会Root你的设备，按任意键继续
 pause >nul 2>nul
 cls
-echo.����رմ��ڻ��Ҷ������̣�������ڱ��������ʾ[ѡ��]�������밴�»س���������Ҽ����ѡ�񣬷�����ִͣ�������
+echo.请勿关闭窗口或乱动鼠标键盘，如果窗口标题左侧显示[选择]字样，请按下回车或按下鼠标右键清除选择，否则将暂停执行输出。
 echo.
 echo.
 )
-echo.���֡�1��Ϊȷ�ϣ���0��Ϊ����
+echo.数字“1”为确认，“0”为否认
 :Q0
 echo.
 echo.
-set /p CHOICE0="��1����ˢ������0�˳�ˢ��(1/0) "
+set /p CHOICE0="按1再刷一遍，按0退出刷机(1/0) "
 if /i "%CHOICE0%" == "1" (
     goto Q1
 ) else if /i "%CHOICE0%" == "0" (
@@ -30,9 +30,9 @@ set cecho=bin\windows\all\cecho.exe
 goto MAIN
 :MAIN
 echo.
-echo.===��ʼ����Bootloader��ˢ�����...
+echo.===开始解锁Bootloader并刷入面具和共存recovery...
 echo.
-echo.===�����ĵȴ���ˢ�������������..
+echo.===请耐心等待，刷机过程请勿操作..
 echo.
 bin\windows\all\adb reboot fastboot
 bin\windows\all\fastboot %* devices
